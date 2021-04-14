afb1500616113d591aa15aab7227c23799bf8466
Overfair <adilet.askarov1@gmail.com>
2021-04-14 17:55:49 +0600

    Added Review + Item, changes in AdmDivision (finished)

--- src/Catalog/Items/AdmDivision.php
+++ src/Catalog/Items/AdmDivision.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 namespace Kuvardin\DoubleGis\Catalog\Items;
 
 use Kuvardin\DoubleGis\Catalog\AdmDivision as AdmDivisionShort;
+use Kuvardin\DoubleGis\Catalog\Flags;
 use Kuvardin\DoubleGis\Catalog\Item;
+use Kuvardin\DoubleGis\Catalog\Items\Building\Context;
+use Kuvardin\DoubleGis\Catalog\Links\Links;
+use Kuvardin\DoubleGis\Catalog\Reviews\Review;
+use Kuvardin\DoubleGis\Catalog\SearchAttributes;
+use Kuvardin\DoubleGis\Geometry\Geometry;
+use Kuvardin\DoubleGis\Geometry\Point;
 use Kuvardin\DoubleGis\Traits\LongId;
 
 /**
- * Class AdmDivision
+ * Геообъект типа административная единица
  *
  * @package Kuvardin\DoubleGis\Catalog\Items
  * @author Maxim Kuvardin <maxim@kuvard.in>
@@ -19,12 +26,12 @@ class AdmDivision extends Item
     use LongId;
 
     /**
-     * @var string Идентификатор региона
+     * @var string|null Идентификатор региона
      */
-    public string $region_id;
+    public ?string $region_id = null;
 
     /**
-     * @var string|null Уникальный идентификатор сегмента
+     * @var string|null Уникальный идентификатор сегмента 14561
      */
     public ?string $segment_id = null;
 
@@ -32,6 +39,26 @@ class AdmDivision extends Item
      * @var string Подтип административной единицы (AdmDivision\Types::*)
      */
     public string $subtype;
+
+    /**
+     * @var Flags|null Список признаков объекта
+     */
+    public ?Flags $flags = null;
+
+    /**
+     * @var Context|null
+     */
+    public ?Context $context = null;
+
+    /**
+     * @var string|null Алиас города, в котором находится объект
+     */
+    public ?string $city = null;
+
+    /**
+     * @var string|null Текущая локаль
+     */
+    public ?string $locale = null;
 
     /**
      * @var string|null Локализованное название типа населённого пункта (только для subtype = settlement)
@@ -44,10 +71,10 @@ class AdmDivision extends Item
     public ?bool $is_main_in_group = null;
 
     /**
-     * @var string|null Название территории
+     * @var string Название территории
      * (для использования в функционале «поделиться», для конечных точек маршрута и т. д.)
      */
-    public ?string $caption = null;
+    public string $caption;
 
     /**
      * @var string Название территории
@@ -55,9 +82,9 @@ class AdmDivision extends Item
     public string $name;
 
     /**
-     * @var string Полное название территории
+     * @var string|null Полное название территории
      */
-    public string $full_name;
+    public ?string $full_name = null;
 
     /**
      * @var string|null Описание
@@ -65,14 +92,64 @@ class AdmDivision extends Item
     public ?string $description = null;
 
     /**
-     * @var bool Если true, то проезд до объекта возможен, если false или отсутствует, то нет.
+     * @var bool|null Если true, то проезд до объекта возможен, если false или отсутствует, то нет.
      */
-    public bool $is_routing_available;
+    public ?bool $is_routing_available = null;
 
     /**
      * @var AdmDivisionShort[]|null Принадлежность к административной территории более высокого уровня
      */
     public ?array $adm_divs = null;
+
+    /**
+     * @var Links|null Связанные объекты
+     */
+    public ?Links $links = null;
+
+    /**
+     * @var Review|null Отзывы о геообъекте.
+     */
+    public ?Review $reviews;
+
+    /**
+     * @var Point|null Координаты точки поиска, заданные в системе координат WGS84 в формате lon, lat.
+     */
+    public ?Point $point = null;
+
+    /**
+     * @var SearchAttributes|null Подсказка, соответствующая запросу. Поле доступно только в методах автодополнения.
+     */
+    public ?SearchAttributes $search_attributes = null;
+
+    /**
+     * @var Attraction|null Описание достопримечательности.
+     */
+    public ?Attraction $attraction = null;
+
+    /**
+     * @var Statistics|null Сводная информация об административной единице.
+     */
+    public ?Statistics $statistics = null;
+
+    /**
+     * @var Geometry|null Геометрия территории
+     */
+    public ?Geometry $geometry = null;
+
+    /**
+     * @var array|null Идентификатор источника данных об объекте. Если отсутсвует, источник данных — 2GIS.
+     */
+    public ?array $sources = null;
+
+    /**
+     * @var ExternalContent[]|null Внешний контент
+     */
+    public ?array $external_content = null;
+
+    /**
+     * @var Dates|null Время внесения информации о филиале в БД.
+     */
+    public ?Dates $dates = null;
 
     /**
      * AdmDivision constructor.
@@ -88,12 +165,25 @@ class AdmDivision extends Item
         $this->subtype = $data['subtype'];
         $this->subtype_specification = $data['subtype_specification'] ?? null;
         $this->is_main_in_group = $data['is_main_in_group'] ?? null;
-        $this->caption = $data['caption'] ?? null;
         $this->name = $data['name'];
+        $this->caption = $data['caption'];
         $this->full_name = $data['full_name'];
         $this->description = $data['description'] ?? null;
         $this->is_routing_available = $data['is_routing_available'];
-
+        $this->attraction = $data['attraction'] ?? null;
+        $this->city = $data['city'] ?? null;
+        $this->context = $data['context'] ?? null;
+        $this->dates = $data['dates'] ?? null;
+        $this->external_content = $data['external_content'] ?? null;
+        $this->flags = $data['flags'] ?? null;
+        $this->geometry = $data['geometry'] ?? null;
+        $this->links = $data['links'] ?? null;
+        $this->locale = $data['locale'] ?? null;
+        $this->point = $data['point'] ?? null;
+        $this->reviews = $data['reviews'] ?? null;
+        $this->search_attributes = $data['search_attributes'] ?? null;
+        $this->sources = $data['sources'] ?? null;
+        $this->statistics = $data['statistics'] ?? null;
         if (isset($data['adm_div'])) {
             $this->adm_divs = [];
             foreach ($data['adm_div'] as $adm_div_data) {
@@ -109,6 +199,4 @@ class AdmDivision extends Item
     {
         return Item::TYPE_ADM_DIV;
     }
-
-
 }
---
+++ src/Catalog/Reviews/Review.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Kuvardin\DoubleGis\Catalog\Reviews;
+
+/**
+ * Отзывы о геообъекте.
+ *
+ * @package Kuvardin\DoubleGis\Catalog\Reviews
+ */
+class Review
+{
+    /**
+     * @var bool|null Разрешено ли отображать отзывы к этому объекту, а также создавать новые отзывы.
+     */
+    public ?bool $is_reviewable;
+
+    /**
+     * @var bool|null Разрешено ли оставлять отзывы к данному объекту на сервисе Flamp.
+     */
+    public ?bool $is_reviewable_on_flamp;
+
+    /**
+     * @var Item[]|null Список провайдеров.
+     */
+    public ?array $items;
+
+    /**
+     * @var float|null Рейтинг объекта
+     */
+    public ?float $general_rating;
+
+    /**
+     * @var int|null Общее количество отзывов
+     */
+    public ?int $general_review_count;
+
+    /**
+     * Review constructor.
+     *
+     * @param array $data
+     */
+    public function __construct(array $data)
+    {
+        $this->is_reviewable = $data['is_reviewable'] ?? null;
+        $this->is_reviewable_on_flamp = $data['is_reviewable_on_flamp'] ?? null;
+        $this->items = $data['items'] ?? null;
+        $this->general_rating = $data['general_rating'] ?? null;
+        $this->general_review_count = $data['general_review_count'] ?? null;
+    }
+}
